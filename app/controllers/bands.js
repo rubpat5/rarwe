d5312789f9bb114eccf3eafd36f778dc89b79916
rubpat5 <rub.patvakanyan@gmail.com>
2018-09-14 10:07:37 +0400

    added songs template and logic, star-rating template

---
+++ app/controllers/bands.js
@@ -0,0 +1,30 @@
+import Controller from '@ember/controller';
+import Band from 'rarwe/models/band';
+import { empty } from '@ember/object/computed';
+
+
+export default Controller.extend({
+
+  isAddingBand: false,
+  newBandName: '',
+  isAddButtonDisabled: empty('newBandName'),
+
+  actions: {
+    addBand() {
+      this.set('isAddingBand', true);
+    },
+    cancelAddBand() {
+      this.set('isAddingBand', false);
+    },
+    saveBand(event) {
+      event.preventDefault();
+      const newBand = Band.create({name: this.newBandName});
+      this.model.pushObject(newBand);
+      this.setProperties({
+        newBandName: '',
+        isAddingBand: false,
+      });
+      this.transitionToRoute('bands.band.songs', newBand);
+    }
+  }
+});
